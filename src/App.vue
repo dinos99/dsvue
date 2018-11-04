427ff3332caad3db6c96fc481b5cca24cd89284e
dinos99 <dojun8ho@gmail.com>
2018-11-04 23:06:14 +0900

    Add Header menu

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,24 @@
           clipped-left
           flat
         >
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
             <v-toolbar-title class="ml-0 pl-3 white--text">
-                <span class="title">dsVue v1.0</span>
+                <div class="title">dsVue v1.0</div>
             </v-toolbar-title>
+            <ds-header></ds-header>
             <v-spacer></v-spacer> 
-            <!--
-            <v-avatar size="30">
+            <v-avatar size="30" class="pr-3">
                 <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
             </v-avatar>
-            <div style="padding-left:5px;">dojun8ho@gmail.com</div>
+            <div>dojun8ho@gmail.com</div>
+            <!--
             <v-btn depressed small color="primary">Login</v-btn>
             <v-btn icon><v-icon>search</v-icon></v-btn>
-            <v-btn icon><v-icon>apps</v-icon></v-btn>
             <v-btn icon><v-icon>refresh</v-icon></v-btn>
-            <v-btn icon><v-icon>more_vert</v-icon></v-btn>
             -->
-            <v-btn depressed small flat>Login</v-btn>
+            <v-btn icon><v-icon>notifications</v-icon></v-btn>
+            <v-btn icon><v-icon>apps</v-icon></v-btn>
+            <v-btn icon><v-icon>more_vert</v-icon></v-btn>
         </v-toolbar>
       <router-view/>
     </v-app>
@@ -32,15 +33,16 @@
 </template>
 
 <script>
+import dsHeader from '@/components/frameset/dsHeader'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    dsHeader
+  }
 }
 </script>
 
 <style>
-.v-toolbar {
-  width:  1024px; margin:0 auto;
-}
+.title {width: 240px;}
 #app { width: 100%; margin:0; padding:0; }
-#inspire { width:1024px; margin:0 auto; padding:0;}
 </style>
---
+++ src/components/frameset/dsHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-tabs v-model="acmenu" color="primary" slider-color="white">
+    <v-tab
+      v-for="item in items"
+      :style="'padding:0 15px'"
+      :key="item"
+    >
+      {{ item }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  name: 'dsHeader',
+  data: () => ({
+    acmenu: 0,
+    items: [
+      'Home', 'Blog', 'Study', 'Diary', 'Playground'
+    ]
+  })
+}
+</script>
+
