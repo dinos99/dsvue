72fdeb449210190b7f3711a9ab793b05a22dd438
dinos99 <dojun8ho@gmail.com>
2018-11-05 08:23:20 +0900

    add slide menu

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <v-app id="inspire">
-        <v-toolbar
-          dark
-          color="primary"
-          dense
-          clipped-left
-          flat
-        >
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title class="ml-0 pl-3 white--text">
-                <div class="title">dsVue v1.0</div>
-            </v-toolbar-title>
-            <ds-header></ds-header>
-            <v-spacer></v-spacer> 
-            <v-avatar size="30" class="pr-3">
-                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
-            </v-avatar>
-            <div>dojun8ho@gmail.com</div>
-            <!--
-            <v-btn depressed small color="primary">Login</v-btn>
-            <v-btn icon><v-icon>search</v-icon></v-btn>
-            <v-btn icon><v-icon>refresh</v-icon></v-btn>
-            -->
-            <v-btn icon><v-icon>notifications</v-icon></v-btn>
-            <v-btn icon><v-icon>apps</v-icon></v-btn>
-            <v-btn icon><v-icon>more_vert</v-icon></v-btn>
-        </v-toolbar>
+    <v-app id="inspire" dark>
+      <v-toolbar
+        dark
+        color="primary"
+        dense
+        clipped-left
+        flat
+      >
+          <v-toolbar-side-icon @click.stop="display.sidemenu = !display.sidemenu"></v-toolbar-side-icon>
+          <v-toolbar-title :style="'width:340px;'" class="ml-0 pl-1 pr-3 white--text">
+              <div class="title">다구형 놀이터 v1.0</div>
+          </v-toolbar-title>
+          <ds-header></ds-header>
+          <v-spacer></v-spacer> 
+          <v-avatar size="30" class="pr-3">
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+          </v-avatar>
+          <div>dojun8ho@gmail.com</div>
+          <!--
+          <v-btn depressed small color="primary">Login</v-btn>
+          <v-btn icon><v-icon>search</v-icon></v-btn>
+          <v-btn icon><v-icon>refresh</v-icon></v-btn>
+          -->
+          <v-btn icon><v-icon>notifications</v-icon></v-btn>
+          <v-btn icon><v-icon>apps</v-icon></v-btn>
+          <v-btn icon><v-icon>more_vert</v-icon></v-btn>
+      </v-toolbar>
+      <ds-side-menu
+        :drawer="display.sidemenu">
+      </ds-side-menu>
       <router-view/>
     </v-app>
   </div>
@@ -34,15 +37,21 @@
 
 <script>
 import dsHeader from '@/components/frameset/dsHeader'
+import dsSideMenu from '@/components/frameset/dsSideMenu'
 export default {
   name: 'App',
   components: {
-    dsHeader
-  }
+    dsHeader,
+    dsSideMenu
+  },
+  data: () => ({
+    display: {
+      sidemenu: null
+    }
+  })
 }
 </script>
 
 <style>
-.title {width: 240px;}
-#app { width: 100%; margin:0; padding:0; }
+.title {font-family: 'Nanum Gothic' !important; font-weight: bold;}
 </style>
--- src/components/frameset/dsHeader.vue
+++ src/components/frameset/dsHeader.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-tabs v-model="acmenu" color="primary" slider-color="white">
-    <v-tab
-      v-for="item in items"
-      :style="'padding:0 15px'"
-      :key="item"
-    >
-      {{ item }}
-    </v-tab>
-  </v-tabs>
+  <div class="topHeaderWrap">
+    <div class="topHeader">
+      <v-tabs v-model="acmenu" color="primary" slider-color="white">
+        <v-tab
+          v-for="item in items"
+          :style="'padding:0 15px'"
+          :key="item"
+        >
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,4 +25,8 @@ export default {
   })
 }
 </script>
+<style>
+  .topHeaderWrap { width: 100%;}
+  .topHeader { width: 600px; max-width:700px; margin:0 auto;}
+</style>
 
