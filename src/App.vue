abc1869a0b1c3a924e11ed6c651a9a0aca0356b4
dinos99 <dojun8ho@gmail.com>
2018-11-04 21:48:47 +0900

    App vue Test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <div id="app">
+    <v-app id="inspire">
+        <v-toolbar
+          dark
+          color="primary"
+          dense
+          clipped-left
+          flat
+        >
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="ml-0 pl-3 white--text">
+                <span class="title">dsVue v1.0</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer> 
+            <!--
+            <v-avatar size="30">
+                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+            </v-avatar>
+            <div style="padding-left:5px;">dojun8ho@gmail.com</div>
+            <v-btn depressed small color="primary">Login</v-btn>
+            <v-btn icon><v-icon>search</v-icon></v-btn>
+            <v-btn icon><v-icon>apps</v-icon></v-btn>
+            <v-btn icon><v-icon>refresh</v-icon></v-btn>
+            <v-btn icon><v-icon>more_vert</v-icon></v-btn>
+            -->
+            <v-btn depressed small flat>Login</v-btn>
+        </v-toolbar>
       <router-view/>
+    </v-app>
   </div>
 </template>
 
@@ -11,4 +38,9 @@ export default {
 </script>
 
 <style>
+.v-toolbar {
+  width:  1024px; margin:0 auto;
+}
+#app { width: 100%; margin:0; padding:0; }
+#inspire { width:1024px; margin:0 auto; padding:0;}
 </style>
--- src/pages/test/Test.vue
+++ src/pages/test/Test.vue
@@ -1,29 +1,4 @@
 <template>
-<v-app id="inspire">
-    <v-toolbar
-        dark
-        color="primary"
-        dense
-        fixed
-        clipped-left
-        app
-        flat
-    >
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title class="ml-0 pl-3 white--text">
-            <span class="title">dsVue v1.0</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer> 
-        <v-avatar size="30">
-            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
-        </v-avatar>
-        <div style="padding-left:5px;">dojun8ho@gmail.com</div>
-        <v-btn icon><v-icon>search</v-icon></v-btn>
-        <v-btn icon><v-icon>apps</v-icon></v-btn>
-        <v-btn icon><v-icon>refresh</v-icon></v-btn>
-        <v-btn icon><v-icon>more_vert</v-icon></v-btn>
-    </v-toolbar>
-</v-app>
 </template>
 <script>
  export default {
